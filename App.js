fb794466871f61881e2f2ca2a8cccbabe75bcf39
javier-deep <152357887+javier-deep@users.noreply.github.com>
2025-03-27 14:06:39 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+// App.js
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+import FastImage from 'react-native-fast-image';
+
+
+
+const App = () => {
+  return <AppNavigator />;
+};
+
+export default App;
