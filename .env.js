1ecf85f8f3653c58c4b46416e539a4e3ed84863c
javier-deep <152357887+javier-deep@users.noreply.github.com>
2025-03-31 09:49:02 -0600

    Delete .env.js

--- .env.js
+++
@@ -1,5 +0,0 @@
-
-
-MONGO_URI="mongodb+srv://mewfoodcontacto:mewfood@cluster0.somvq.mongodb.net/mewfood"
-JWT_SECRET="mongodb+srv://mewfoodcontacto:mewfood@cluster0.somvq.mongodb.net/mewfood"
-PORT=3000
